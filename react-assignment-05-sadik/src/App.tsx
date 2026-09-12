7228c2c2e82b17d2b1d6d516c8aea854b8986934
Sadik Ahmed <sadik.ahmed.3806@gmail.com>
2026-09-12 09:21:24 +0600

    project and navbar created

---
+++ react-assignment-05-sadik/src/App.tsx
@@ -0,0 +1,35 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        {/* Temporary content for testing the sticky navbar */}
+        <section
+          id="home"
+          className="flex min-h-[1200px] items-center justify-center px-6"
+        >
+          <div className="text-center">
+            <h1 className="text-4xl font-bold text-gray-900">
+              Dev Stack
+            </h1>
+
+            <p className="mt-4 text-gray-500">
+              Navbar preview area
+            </p>
+          </div>
+        </section>
+
+        {/* Temporary anchor targets */}
+        <section id="technologies" className="h-20" />
+        <section id="projects" className="h-20" />
+        <section id="about" className="h-20" />
+        <section id="contact" className="h-20" />
+      </main>
+    </>
+  );
+}
+
+export default App;
