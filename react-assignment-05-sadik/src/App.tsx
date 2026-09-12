2ec98aeb1fa272d91a3283993fccc00c5cf7fb7b
Sadik Ahmed <sadik.ahmed.3806@gmail.com>
2026-09-12 10:03:23 +0600

    components installed

--- react-assignment-05-sadik/src/App.tsx
+++ react-assignment-05-sadik/src/App.tsx
@@ -7,23 +7,9 @@ function App() {
       <Navbar />
       <Hero />
 
-
       <main>
-        {/* Temporary content for testing the sticky navbar */}
-        <section
-          id="home"
-          className="flex min-h-[1200px] items-center justify-center px-6"
-        >
-          <div className="text-center">
-            <h1 className="text-4xl font-bold text-gray-900">
-              Dev Stack
-            </h1>
-
-            <p className="mt-4 text-gray-500">
-              Navbar preview area
-            </p>
-          </div>
-        </section>
+       
+      
 
         {/* Temporary anchor targets */}
         <section id="technologies" className="h-20" />
